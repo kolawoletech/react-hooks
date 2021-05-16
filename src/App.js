6eeb60d4751892ca93f82e34d47469b8a31200e8
Kolawole Olulana <kolawolegolulana@gmail.com>
2021-05-16 14:19:38 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { useForm } from "./useForm";
+//import { Hello } from "./Hello";
+import { useFetch } from "./useFetch";
 
-function App() {
+const App = () => {
+  const [values, handleChange] = useForm({
+    email: "",
+    password: "",
+    firstName: ""
+  });
+  const [count, setCount] = useState(() =>
+    JSON.parse(localStorage.getItem("count"))
+  );
+  // USE EFFECT IMPORT
+  const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
+
+  useEffect(() => {
+    localStorage.setItem("count", JSON.stringify(count));
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{!data ? "loading..." : data}</div>
+      <div>count: {count}</div>
+      <button onClick={() => setCount(c => c + 1)}>increment</button>
+      <>
+        {/* <button onClick={() => setShowHello(!showHello)}>toggle</button> */}
+        {/* {showHello && <Hello />} */}
+        <input name="email" value={values.email} onChange={handleChange} />
+        <input
+          name="firstName"
+          placeholder="first name"
+          value={values.firstName}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+        />
+      </>
     </div>
   );
-}
+};
 
 export default App;
